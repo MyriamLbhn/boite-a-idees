71694ec6a65efc76b8415564670032e53b36c0bd
MyriamLbhn <lebihan.myriam@gmail.com>
2023-03-24 15:07:41 +0100

    add vote possibility

--- boite_idees/userapp/models.py
+++ boite_idees/userapp/models.py
@@ -5,7 +5,7 @@ class Idea(models.Model):
     formulation = models.CharField(max_length=100)
     detail = models.CharField(max_length=200, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Voter(models.Model):
     voter = models.ForeignKey(User, on_delete=models.CASCADE)
--- boite_idees/userapp/views.py
+++ boite_idees/userapp/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from userapp.forms import UserCreationFormCustom
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-
+from django.db.models import Sum
+from django.contrib import messages
+from django.db.models import Sum
 
 from .models import Idea, Voter
 from .forms import IdeaForm
@@ -13,29 +15,41 @@ class SignupPage(CreateView):
     template_name = 'userapp/signup.html'
     form_class = UserCreationFormCustom
 
+
+@login_required
 def home(request):
+    liste_idee = Idea.objects.all()
+    for idee in liste_idee:
+        idee.total_votes = Voter.objects.filter(idea=idee).aggregate(Sum('vote'))['vote__sum']
+    context = {'liste_idee': liste_idee}
+    if request.method == 'POST':
+        vote_value = int(request.POST.get('vote_value'))
+        idea_id = int(request.POST.get('idea_id'))
+        idea = get_object_or_404(Idea, pk=idea_id)
+        voter, created = Voter.objects.get_or_create(voter=request.user, idea=idea)
+        if created:
+            voter.vote = vote_value
+            voter.save()
+        elif voter.vote == vote_value:
+            voter.delete()
+        else:
+            voter.vote = vote_value
+            voter.save()
+        return redirect('userapp/home')
+    return render(request, 'userapp/home.html', context)
 
-    ideas = Idea.objects.all()
 
-    return render(request, 'userapp/home.html',{'liste_idee': ideas})
-
-def login_view(request):
-    return render(request, 'userapp/login.html')
-
-def logout_view(request):
-    return render(request, 'userapp/logout.html')
-
-    
 @login_required
 def ajouter_idee(request):
     if request.method == 'POST':
         form = IdeaForm(request.POST)
         if form.is_valid():
-            idea = form.save(commit=False)
-            idea.author = request.user
-            idea.save()
+            nouvelle_idee = form.save(commit=False)
+            nouvelle_idee.author = request.user
+            nouvelle_idee.save()
             return redirect('home')
     else:
         form = IdeaForm()
-    return render(request, 'userapp/ajouter_idee.html', {'form': form})
+    context = {'form': form}
+    return render(request, 'userapp/ajouter_idee.html', context)
 
